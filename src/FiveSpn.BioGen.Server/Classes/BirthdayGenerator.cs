639ecdea69bdba9b8ca5bf94d6ee1a4ec9faadac
SourPatchNom <11154163+SourPatchNom@users.noreply.github.com>
2022-11-09 18:11:08 -0600

    refactor(files):move to proper structure

---
+++ src/FiveSpn.BioGen.Server/Classes/BirthdayGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FiveSpn.BioGen.Server.Classes
+{
+    public static class BirthdayGenerator
+    {
+        public static DateTime GetRandomAdultBirthday()
+        {
+            var utcNow = DateTime.UtcNow;
+            var old = new DateTime(utcNow.Year - 65, utcNow.Month, utcNow.Day);
+            var young = new DateTime(utcNow.Year - 18, utcNow.Month, utcNow.Day);
+            var maxDaysToAdd = (utcNow.Date - old).Days - (utcNow.Date - young).Days;
+            return old.AddDays(GlobalRandom.GetRandomNumberInRange(0, maxDaysToAdd));
+        }
+        
+    }
+}
